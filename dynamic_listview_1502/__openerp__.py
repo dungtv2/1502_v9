cdf8b09bedba671cf79309550b476901157b3d26
dungtv2 <dungtv2@hanelsoft.vn>
2017-02-15 16:54:24 +0700

    f

--- dynamic_listview_1502/__openerp__.py
+++ dynamic_listview_1502/__openerp__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Dynamic ListView Advance Odoo 9',
+    'name': 'Odoo V9 - ListView Dynamic Advance',
     'summary': 'Change The Odoo List view On the fly without any technical knowledge',
     'version': '1.0',
     'category': 'Web',
